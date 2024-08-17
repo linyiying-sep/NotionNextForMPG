9ffa0b5f21014a28e02ec61f03557ff70967c692
linyiying-sep <s410276133@gm.ntpu.edu.tw>
2024-08-17 22:05:07 +0800

    PostCopyright.js 註銷顯示版權的代碼

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -16,7 +16,7 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '寵物用品',
   HEO_HERO_TITLE_2: '就找喵派',
-  HEO_HERO_TITLE_3: 'Ins：@meowpawsgoodies',
+  HEO_HERO_TITLE_3: 'INS：@meowpawsgoodies',
   HEO_HERO_TITLE_4: '熱門商品',
   HEO_HERO_TITLE_5: '好物推薦',
   HEO_HERO_TITLE_LINK: 'https://meowpawsgoodies.com',
@@ -27,7 +27,7 @@ const CONFIG = {
   HEO_HERO_CATEGORY_3: { title: '新品快訊', url: '/tag/新品快訊' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '推荐',
+  HEO_HERO_RECOMMEND_POST_TAG: '推薦',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
