0fca90fa519d42e96d08a92b351cb567cdfa880d
linyiying-sep <s410276133@gm.ntpu.edu.tw>
2024-08-18 21:04:15 +0800

    footer 颜色更改

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = ({ title }) => {
     parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
   return (
-    <footer className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'>
+    <footer className='relative flex-shrink-0 bg-[#fffaed] dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'>
       {/* 颜色过度区 */}
       <div
         id='color-transition'
