a11faf8583a7ddd7bfa3757163e303509b9c64b9
linyiying-sep <s410276133@gm.ntpu.edu.tw>
2024-08-19 16:48:09 +0800

    change left hero bg color to #4c3b3061

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -97,7 +97,7 @@ function Banner(props) {
         id='banner-cover'
         style={{ backdropFilter: 'blur(15px)' }}
         className={
-          'rounded-xl overflow-hidden opacity-0 group-hover:opacity-100 duration-300 transition-all bg-[#4259efdd] dark:bg-[#dca846] dark:text-white cursor-pointer absolute w-full h-full top-0 flex justify-start items-center'
+          'rounded-xl overflow-hidden opacity-0 group-hover:opacity-100 duration-300 transition-all bg-[#4c3b3061] dark:bg-[#dca846] dark:text-white cursor-pointer absolute w-full h-full top-0 flex justify-start items-center'
         }>
         <div className='ml-12 -translate-x-32 group-hover:translate-x-0 duration-300 transition-all ease-in'>
           <div className='text-7xl text-white font-extrabold'>隨意逛逛</div>
