40c415db7fd84dc03e67d5e1718866b7aa590873
linyiying-sep <s410276133@gm.ntpu.edu.tw>
2024-08-24 19:38:39 +0800

    change  {/* 文章Title */}  {/* 标题底部补充信息 */} color

--- themes/heo/components/PostHeader.js
+++ themes/heo/components/PostHeader.js
@@ -81,10 +81,10 @@ export default function PostHeader({ post, siteInfo, isDarkMode }) {
                     href={`/tag/${encodeURIComponent(tag.name)}`}
                     passHref
                     className={
-                      'cursor-pointer inline-block text-[#fdf6e3] hover:text-white duration-200 py-0.5 px-1 whitespace-nowrap '
+                      'cursor-pointer inline-block text-[#4c3b30] hover:text-white duration-200 py-0.5 px-1 whitespace-nowrap '
                     }>
                     <div className='font-light flex items-center'>
-                      <HashTag className='text-gray-200 stroke-2 mr-0.5 w-3 h-3' />{' '}
+                      <HashTag className='text-[#4c3b30] stroke-2 mr-0.5 w-3 h-3' />{' '}
                       {tag.name + (tag.count ? `(${tag.count})` : '')}{' '}
                     </div>
                   </Link>
