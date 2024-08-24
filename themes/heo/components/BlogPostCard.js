a6f313ca9d5b14408d29b07f482535ba841e7045
linyiying-sep <s410276133@gm.ntpu.edu.tw>
2024-08-25 00:01:58 +0800

    change 產品圖片大小

--- themes/heo/components/BlogPostCard.js
+++ themes/heo/components/BlogPostCard.js
@@ -25,7 +25,7 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
       <div
         data-wow-delay='.2s'
         className={
-          'wow fadeInUp border bg-white dark:bg-[#1e1e1e] flex mb-4 flex-col h-[23rem] md:h-52 md:flex-row 2xl:h-96 2xl:flex-col group w-full dark:border-gray-600 hover:border-indigo-600  dark:hover:border-yellow-600 duration-300 transition-colors justify-between overflow-hidden rounded-xl'
+          'wow fadeInUp border bg-white dark:bg-[#1e1e1e] flex mb-4 flex-col h-[20rem] md:h-52 md:flex-row 2xl:h-96 2xl:flex-col group w-full dark:border-gray-600 hover:border-indigo-600  dark:hover:border-yellow-600 duration-300 transition-colors justify-between overflow-hidden rounded-xl'
         }>
         {/* 图片封面 */}
         {showPageCover && (
