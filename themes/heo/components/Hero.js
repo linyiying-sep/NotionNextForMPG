78a24377f1ec293a20df6bde1813b3b59841c6c6
linyiying-sep <s410276133@gm.ntpu.edu.tw>
2024-09-15 17:29:10 +0800

    Hero 修复新品快讯颜色

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -185,7 +185,7 @@ function GroupMenu() {
        {/* 第二个标签在小屏上不显示 */}
       <Link
         href={url_2}
-        className='group relative overflow-hidden bg-gradient-to-r from-rose-400 to-rose-200 hidden h-20 xl:flex justify-start items-center text-white rounded-xl xl:hover:w-1/2 xl:w-1/3 transition-all duration-500 ease-in'>
+        className='group relative overflow-hidden bg-gradient-to-r from-red-500 to-rose-200 hidden h-20 xl:flex justify-start items-center text-white rounded-xl xl:hover:w-1/2 xl:w-1/3 transition-all duration-500 ease-in'>
         <div className='font-bold text-lg pl-5 relative -mt-2'>
           {title_2}
           <span className='absolute -bottom-0.5 left-5 w-5 h-0.5 bg-white rounded-full'></span>
